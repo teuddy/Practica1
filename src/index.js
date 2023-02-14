9a92280e251c57914cbd05dd2876db414372101b
rafaelrocky <125231774+rafaelrocky@users.noreply.github.com>
2023-02-14 11:33:25 -0400

    hoy aprendi a hacer objetos y a abrir un nuevo end point

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
 
+
+const persona = {
+    edad : 34,
+    nombre : "pitufina",
+    altura : 2.00,
+}
+
+
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.json(persona)
 });
 
-app.listen(3000);
+
+app.get("/persona", function (req, res){
+    res.send("rafael hizo esto")
+})
+
+
+app.listen(3000)
+
+
+
