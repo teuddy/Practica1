a5f36217e19ea4e0fc78f684d7f5ba1568d8b8bc
teuddycr <88355800+teuddycr@users.noreply.github.com>
2023-02-14 11:03:53 -0400

    config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hola gente, pronto este sera un gran proyecto");
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(3000);
